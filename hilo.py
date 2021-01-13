6adf4222b6a5141196e71933c9b93e50801de88c
griffinbeef <willampetergriffin@gmail.com>
2021-01-13 11:05:56 -0700

    Enter

--- hilo.py
+++ hilo.py
@@ -3,5 +3,19 @@ class Deal:
         self.cards = []
         self.score = 0
 
+    def new_card(self):
+
+
 class Hilo:
     def __init__(self):
+        pass
+
+    def start_game(self):
+        deal = Deal()
+        deal.new_card()
+        playing = True
+        while playing:
+            print("The card is: ") #insert card number here
+            print("Higher or lower? [h/l] ") #get user input here
+            print("Your score is: ") #print calculated score
+            print("Keep playing? [y/n] ") #get user input here
