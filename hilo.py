d39ab29b6aff8619aeaea48b0c262d3614bc151f
griffinbeef <willampetergriffin@gmail.com>
2021-01-13 11:27:46 -0700

    Enter

--- hilo.py
+++ hilo.py
@@ -1,9 +1,15 @@
+import random
+
 class Deal:
     def __init__(self):
         self.cards = []
         self.score = 0
 
     def new_card(self):
+        pass #for now
+
+    def guess(self):
+        pass #for now
 
 
 class Hilo:
@@ -17,5 +23,17 @@ class Hilo:
         while playing:
             print("The card is: ") #insert card number here
             print("Higher or lower? [h/l] ") #get user input here
-            print("Your score is: ") #print calculated score
-            print("Keep playing? [y/n] ") #get user input here
+            deal.new_card()
+
+            # Here the new card is compared to the guess
+
+            print("Your score is: ", deal.score) #print calculated score
+            if deal.score > 0:
+                playing = input("Keep playing? [y/n] ") in ['y']
+            else:
+                print("Sorry! Game over!")
+                playing = False
+
+if __name__ == "__main__":
+    hilo = Hilo()
+    hilo.start_game()
