c1b54f4310c364903b11bcb669401f9e8185d965
griffinbeef <willampetergriffin@gmail.com>
2021-01-19 15:13:07 -0700

    enter

---
+++ jumper_template/jumper/game/director.py
@@ -0,0 +1,59 @@
+
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.console = Console()
+        self.keep_playing = True
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
+        
+    def do_updates(self):
+        """Updates the important game information for each round of play.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
+        
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
