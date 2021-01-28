9fa7caa6b57218b547c8c5ba125ec3e0cd5356ea
griffinbeef <willampetergriffin@gmail.com>
2021-01-27 19:13:00 -0700

    imported the director class from nim and changed some things

---
+++ cse210-tc04/mastermind/game/driver.py
@@ -0,0 +1,99 @@
+# **copied from Nim** -with minor adjustments
+from game.answer import Answer
+from game.console import Console
+from game.hint import Hint
+from game.player import Player
+from game.result import Result
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        board (Hunter): An instance of the class of objects known as Board.
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        move (Rabbit): An instance of the class of objects known as Move.
+        roster (Roster): An instance of the class of objects known as Roster.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._answer = Answer()
+        self._console = Console()
+        self._keep_playing = True
+        self._move = None
+        self._result = Result()
+        # Do we need to create an instance of hint?
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._prepare_game()
+        while self._keep_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+
+    def _prepare_game(self):
+        """Prepares the game before it begins. In this case, that means getting the player names and adding them to the roster.
+        
+        Args:
+            self (Director): An instance of Director.
+        """
+        for n in range(2):
+            name = self._console.read(f"Enter a name for player {n + 1}: ")
+            player = Player(name)
+            self._roster.add_player(player) # do we need the roster class? or is that what the result class is for? -should we delete this line?
+    
+    def _get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means getting the move from the current player.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        # display the game board
+        board = self._board.to_string() #change to the class we use to create the game
+        self._console.write(board)
+        # get next player's move
+        player = self._roster.get_current() #change depending on if we use roster
+        self._console.write(f"{player.get_name()}'s turn:")
+        number = self._console.read_number("What is your guess? ") # already changed
+        guess = Move(number) #change to assign to class 
+        player.set_move(move) #change to send off
+
+    def _do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the board with the current move.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        player = self._roster.get_current()   #all three of these will have to change
+        move = player.get_move()
+        self._board.apply(move)
+ 
+    def _do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means checking if there are stones left and declaring the winner.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if self._board.is_full(): #changed to is_full
+            winner = self._roster.get_current()
+            name = winner.get_name()
+            print(f"\n{name} won!")
+            self._keep_playing = False
+        self._roster.next_player()
