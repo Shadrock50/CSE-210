f361690eb38cb5999318a5e85c96a417d38cc7a0
griffinbeef <willampetergriffin@gmail.com>
2021-01-30 13:36:12 -0700

    comments

--- cse210-tc04/mastermind/game/display.py
+++ cse210-tc04/mastermind/game/display.py
@@ -1,8 +1,23 @@
 class Display:
+    """ Display controls the displaying of the board and winner
+    
+    Stereotype:
+        Converts output to string
+
+    Attributes:
+        self(Display): instance of display
+        """
     def __init__(self):
-        displayText = ''
+        self.displayText = ""
 
     def displayMain(self, roster, board):
+        """Displays the board
+
+    Args:
+    self(Display): instance of display
+        roster(Roster): instance of roster
+        board(Board): instance of board
+    """
 
         self.displayText = ""
 
@@ -14,6 +29,12 @@ class Display:
         return self.displayText
 
     def displayWinner(self, winnerName):
+        """Displays the winner
+
+    Args:
+        self(Display): instance of display
+        winnerName(str): name of the winner
+    """
 
         self.displayText = ""
 
