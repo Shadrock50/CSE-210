f267af60660ba634fdfddb4e44fcdfc9c0213aac
griffinbeef <willampetergriffin@gmail.com>
2021-02-04 20:59:10 -0700

    comments

--- cse210-tc05/speed/game/word.py
+++ cse210-tc05/speed/game/word.py
@@ -4,8 +4,8 @@ from game.actor import Actor
 from game.point import Point
 
 class Word(Actor):
-    """Food is a nutritious substance that snakes like. The responsibility of Food is to keep track of its appearance
-         and position. Food can move around randomly if asked to do so.
+    """Word is the word that the user must type. The responsibility of Word is to keep track of its appearance
+         and position. 
 
     Stereotype:
         Structurer, Information Holder
@@ -23,19 +23,22 @@ class Word(Actor):
         self._points = 0
         self._word = constants.LIBRARY[random.randint(0 , len(constants.LIBRARY))]
         self.set_text(self._word) 
-        self.set_velocity(constants.MIN_V, constants.MAX_V)
+        self.set_velocity(random.randint(constants.MIN_V, constants.MAX_V))
         self.reset()
 
     def get_points(self):
         return self._points
 
     def reset(self):
-        """changes the position to a random one within the boundaries of the screen and reassigns the points to a random
-             number between 1 and 5.
+        """changes the position to a random one within the boundaries of the screen and reassigns the points to 
+            the length of the word typed.
         
         Args:
             position (integer(s)): the position to change.
             points (integer): The points to add.
+
+        Returns:
+            
         """
         self._points = len(self._word) #does this work for a stretch goal?
         x = random.randint(1, constants.MAX_X - 2)
