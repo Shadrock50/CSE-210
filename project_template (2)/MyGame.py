d1c83b87bc00f2ef067480a380c1d5492467947b
Shadrock50 <shadchristopherson@gmail.com>
2021-03-03 17:37:00 -0700

    Fixed a thing

--- project_template (2)/MyGame.py
+++ project_template (2)/MyGame.py
@@ -65,9 +65,9 @@ class MyGame(arcade.Window):
     def on_key_release(self, key, modifiers):
 
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 1
+            self.player_sprite.change_y = 0
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 1
+            self.player_sprite.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
