971658ab35457821f5604685858cdacfbdb690ed
griffinbeef <willampetergriffin@gmail.com>
2021-03-05 13:46:41 -0700

    changes done together

--- project_template (2)/MyGame.py
+++ project_template (2)/MyGame.py
@@ -8,10 +8,13 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
-        self.coin_list = None
+        self.power_ups_list = None
         self.wall_list = None
         self.player_list = None #May need enemy list
         self.enemy_list = None #May want list for each enemy type
+        self.background_list = None
+        self.flag_list = None
+        self.bullet_list = None
 
         self.player_sprite = None
 
@@ -19,11 +22,11 @@ class MyGame(arcade.Window):
         self.view_left = 0
 
         self.score = 0
+        self.powerup = 0
         self.level = 1
         self.end_of_map = 0
 
          # Load sounds
-        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
         self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
         self.game_over = arcade.load_sound(":resources:sounds/gameover1.wav")
 
@@ -37,8 +40,11 @@ class MyGame(arcade.Window):
 
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+        self.power_ups_list = arcade.SpriteList(use_spatial_hash=True)
         self.enemy_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+        self.background_list = arcade.SpriteList(use_spatial_hash=True)
+        self.flag_list = arcade.SpriteList(use_spatial_hash=True)
 
         image_source = ":resources:images/enemies/mouse.png"
         self.player_sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
@@ -53,7 +59,9 @@ class MyGame(arcade.Window):
         # Name of the layer in the file that has our platforms/walls
         platforms_layer_name = 'Platforms'
         # Name of the layer that has items for pick-up
-        coins_layer_name = 'Coins'
+        power_ups_layer_name = 'Power-ups'
+        enemies_layer_name = 'Enemies'
+        flag_layer_name = 'Flag'
 
         # Read in the tiled map
         my_map = arcade.tilemap.read_tmx(map_name)
@@ -68,7 +76,16 @@ class MyGame(arcade.Window):
                                                       use_spatial_hash=True)
 
         # -- Coins
-        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, constants.TILE_SCALING)
+        self.power_ups_list = arcade.tilemap.process_layer(my_map, power_ups_layer_name, constants.TILE_SCALING)
+
+        # Enemies
+        self.enemies_list = arcade.tilemap.process_layer(my_map, enemies_layer_name, constants.TILE_SCALING)
+
+        # Flag
+        self.flag_list = arcade.tilemap.process_layer(my_map, flag_layer_name, constants.TILE_SCALING)
+
+        # -- Background objects
+        self.background_list = arcade.tilemap.process_layer(my_map, "Background", constants.TILE_SCALING)
 
         # --- Other stuff
         # Set the background color
@@ -91,6 +108,9 @@ class MyGame(arcade.Window):
             self.player_sprite.change_x = -constants.PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.F:
+            self.generate_bullet(self.powerup)
+        
 
     def on_key_release(self, key, modifiers):
 
@@ -109,16 +129,25 @@ class MyGame(arcade.Window):
         # Move the player with the physics engine
         self.physics_engine.update()
 
-        # See if we hit any coins
-        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        self.bullet_list.update()
+        # for bullet in self.bullet_list:
+        #     hit_wall_list = arcade.check_for_collision_with_list(bullet, self.wall_list)
+        #     if len(hit_wall_list) > 0:
+        #         bullet.remove_from_sprite_lists()
+        #     if bullet.left > self.view_left + constants.SCREEN_WIDTH - constants.RIGHT_VIEWPORT_MARGIN:
+        #         bullet.remove_from_sprite_lists()
 
-        # Loop through each coin we hit (if any) and remove it
-        for coin in coin_hit_list:
-            # Remove the coin
-            coin.remove_from_sprite_lists()
-            # Play a sound
-            arcade.play_sound(self.collect_coin_sound)
-            self.score += 1
+
+        # # See if we hit any coins
+        # coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # # Loop through each coin we hit (if any) and remove it
+        # for coin in coin_hit_list:
+        #     # Remove the coin
+        #     coin.remove_from_sprite_lists()
+        #     # Play a sound
+        #     arcade.play_sound(self.collect_coin_sound)
+        #     self.score += 1
 
         changed = False
 
@@ -134,7 +163,7 @@ class MyGame(arcade.Window):
             arcade.play_sound(self.game_over)
 
           # See if the user got to the end of the level
-        if self.player_sprite.center_x >= self.end_of_map:
+        if arcade.check_for_collision_with_list(self.player_sprite, self.flag_list):
             # Advance to the next level
             self.level += 1
 
@@ -181,12 +210,27 @@ class MyGame(arcade.Window):
                                 constants.SCREEN_WIDTH + self.view_left,
                                 self.view_bottom,
                                 constants.SCREEN_HEIGHT + self.view_bottom)
+    
+    def generate_bullet(self, powerup):
+        if powerup == 0:
+            bullet = arcade.Sprite(":resources:images/space_shooter/laserblue01.png", constants.SPRITE_SCALING_LASER)
+            bullet.change_x = constants.BULLET_SPEED
+
+            bullet.left = self.player_sprite.center_y
+            bullet.center_x = self.player_sprite.center_x #position of the bullet
+            self.bullet_list.append(bullet)
+
+
 
     def on_draw(self):
 
         arcade.start_render()
         self.wall_list.draw()
-        self.coin_list.draw()
+        self.background_list.draw()
+        self.bullet_list.draw()
+        self.enemies_list.draw()
+        self.flag_list.draw()
+        self.power_ups_list.draw()
         self.player_list.draw()
 
          # Draw our score on the screen, scrolling it with the viewport
