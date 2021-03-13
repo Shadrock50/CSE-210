e21bf2d837d7a98959a002f5e24a44a22bc9c606
Shadrock50 <shadchristopherson@gmail.com>
2021-03-12 20:12:32 -0700

    Added views. Kill me.

--- project_template (2)/main_game.py
+++ project_template (2)/main_game.py
@@ -1,10 +1,12 @@
 import arcade
 import constants
-from MyGame import MyGame
+from MyGame import FirstView
 
 def main():
-    window = MyGame()
-    window.setup(window.level)
+    
+    window = arcade.Window(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+    start_view = FirstView()
+    window.show_view(start_view)
     arcade.run()
 
 if __name__ == "__main__":
